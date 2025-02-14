60fa2c10b4074a9a4a72d2f71c0376b3858002a3
David Romero San Millán <david.romtrad@gmail.com>
2025-02-14 09:42:40 +0100

    C2-JavaBasics

---
+++ C2-JavaBasics/src/ComentariosApp.java
@@ -0,0 +1,15 @@
+
+public class ComentariosApp {
+
+	public static void main(String[] args) {
+//		TODO Auto-generated method stub
+		/* Comentario multilinea
+		 * 
+		 * Comentario multilinea
+		 */
+		System.out.println("Hola Mundo");
+		System.out.println("Ejemplo 2");
+//Ctrl+7 para generar o quitar lineas de comentario
+	}
+
+}
