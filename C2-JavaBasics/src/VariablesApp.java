ac7058636381f576caa933e17f2f0492be60b0ff
David Romero San Millán <david.romtrad@gmail.com>
2025-02-14 10:42:01 +0100

    Ejercicios varios

---
+++ C2-JavaBasics/src/VariablesApp.java
@@ -0,0 +1,14 @@
+
+public class VariablesApp {
+
+	public static void main(String[] args) {
+		int a = 30; //Declaro el tipo y el nombre de la variable y asigno un valor
+		double b = 30.5; //Declaro el tipo, nombre y asigno el valor
+		a = 50;
+		double suma = a + b;
+		System.out.println(suma);
+//		Escribir "syso" y Ctrl + espacio sugiere directamente la linea de "System.out.println"
+
+	}
+
+}
