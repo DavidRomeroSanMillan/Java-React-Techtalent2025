ca6270d89568c830089c6195ecab43a299ded2d5
David Romero San Millán <david.romtrad@gmail.com>
2025-02-17 12:41:36 +0100

    Update Ejercicio1.java

--- C2-JavaBasics/src/UD4/Ejercicio1.java
+++ C2-JavaBasics/src/UD4/Ejercicio1.java
@@ -10,7 +10,7 @@ public class Ejercicio1 {
 		suma=a+b;
 		div=a/b;
 	System.out.println("El resultado de 5+10 es " +suma+ ", de 5-10 es " +resta+", "
-			+ "de 5*10 es "+mult+", de 5/10 es "+div+"y de 5%10 es "+mod);
+			+ "de 5*10 es "+mult+", de 5/10 es "+div+" y de 5%10 es "+mod);
 	}
 
 }
