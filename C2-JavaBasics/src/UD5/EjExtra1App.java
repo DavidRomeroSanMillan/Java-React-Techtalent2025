5964852c7fed2eab13add6aea859fa57d2758a1e
David Romero San Millán <david.romtrad@gmail.com>
2025-02-25 13:56:37 +0100

    Create EjExtra1App.java

---
+++ C2-JavaBasics/src/UD5/EjExtra1App.java
@@ -0,0 +1,15 @@
+package UD5;
+
+import javax.swing.JOptionPane;
+
+public class EjExtra1App {
+
+	public static void main(String[] args) {
+		
+		int a = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número: "));
+		
+	
+			
+	
+	}
+}
