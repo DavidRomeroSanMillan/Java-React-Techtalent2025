8ef615386b035f74d84db0343dbfcd865caa80b8
David Romero San Millán <david.romtrad@gmail.com>
2025-03-07 14:04:53 +0100

    7/3

--- C2-JavaBasics/src/UD06/Tareas/Ej02NumeroAleatorioApp.java
+++ C2-JavaBasics/src/UD06/Tareas/Ej02NumeroAleatorioApp.java
@@ -7,17 +7,21 @@ public class Ej02NumeroAleatorioApp {
 	public static void main(String[] args) {
 
 		int numVeces = Integer
-				.parseInt(JOptionPane.showInputDialog("¿Cuantos números aleatorios entre el 5 y el 50 quieres?"));
+				.parseInt(JOptionPane.showInputDialog("¿Cuantos números aleatorios quieres?"));
 		int i = 1;
+		
+		int min = Integer
+				.parseInt(JOptionPane.showInputDialog("¿Qué mínimo quieres?"));
+		int max = Integer
+				.parseInt(JOptionPane.showInputDialog("¿Qué máximo quieres?"));
 		while (i <= numVeces) {
 			
-		JOptionPane.showMessageDialog(null, "¡Te ha tocado el número: " + aleatorio() + "!");
+		JOptionPane.showMessageDialog(null, "¡Te ha tocado el número: " + aleatorio(min, max) + "!");
 		i++;}
 	}
 
-	private static int aleatorio() {
-		int min = 5;
-		int max = 50;
+	private static int aleatorio(int min, int max) {
+	
 		int random = (int) (Math.random() * ((max - min) + 1)) + min;
 		return random;
 
--- C2-JavaBasics/src/UD06/Tareas/Ej12Array300.java
+++ C2-JavaBasics/src/UD06/Tareas/Ej12Array300.java
@@ -3,28 +3,33 @@ package UD06.Tareas;
 import javax.swing.JOptionPane;
 
 public class Ej12Array300 {
-//Debemos coger el input del usuario, y en un bucle filtrar los números del array random para que,
+	//Debemos coger el input del usuario, y en un bucle filtrar los números del array random para que,
 	//en otro array, se pongan los que son múltiplos de 5.
+	
+	
 	public static void main(String[] args) {
-
+		int dimension = Integer.parseInt(JOptionPane.showInputDialog("Introduce el último valor que quieres: "));
 		
+            
 		
 	}
 	 public static int[] randomArray1(int[] array1, int dimension) {
 
 	        // Crear un método que haga que los arrays sean creados random
 	        array1 = new int[dimension];
-
+	        if (dimension <= 0) {
+	            System.out.println("El tamaño del array debe ser mayor que 0.");}
+	        else {
 	        for (int i = 0; i < dimension; i++) {
 	            array1[i] = (int) (Math.random() * 300) + 1;
 	        }
-	        return array1;
-	    }
-	public static int [] comparador() {
-	    int ultimoNumero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el último valor que quieres: "));
-		
-	    
-		return null;
-		
-	}
+	        
+	    }return array1;}
+	 
+	 public static int[] comparador(int[] array1, int numero) {
+	        
+	            }
+	        }
+			return array1;
+	 }
 }
