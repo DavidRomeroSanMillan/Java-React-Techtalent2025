f193fd306fdc1c42968c63f9675230da7f639b5d
David Romero San Millán <david.romtrad@gmail.com>
2025-03-17 13:57:17 +0100

    Create Ej01MediaAlumnosApp.java

---
+++ C2-JavaBasics/src/UD07/Tareas/Ej01MediaAlumnosApp.java
@@ -0,0 +1,15 @@
+package UD07.Tareas;
+
+public class Ej01MediaAlumnosApp {
+
+	public static void main(String[] args) {
+		//Crear un método que sirva para añadir alumnos en un arraylist, y otro método que sirva para meter
+		//las notas de estos alumnos en otro arraylist. Estos métodos se irán repitiendo hasta 3 veces (por probar)
+		//y habrá otro método que calcule las medias de los números y las ponga en otro arraylist. 
+		//Podría usar el método get para el cálculo de la media
+		//Usar Scanner, no JOptionPane, será más sencillo de verlo todo a la vez
+		//Recuerda que existe el foreach por favor. Mirar el método iterator
+		//Quizás mirar hashmaps para ver como de viable seria
+	}
+
+}
