1464b8aee085c5c54661375e4d20d3117ca84014
David Romero San Millán <david.romtrad@gmail.com>
2025-04-28 09:23:33 +0200

    Prueba repo vsc 2

--- C2-JavaBasics/src/UD04/Ejercicio1App.java
+++ C2-JavaBasics/src/UD04/Ejercicio1App.java
@@ -3,13 +3,13 @@ package UD04;
 public class Ejercicio1App {
 
 	public static void main(String[] args) {
-		float a = 5, b = 10, suma, resta, div, mult, mod;
+		float a = 5, b = 10, suma, resta, div, multi, mod;
 		mod = a % b;
 		resta = a - b;
-		mult = a * b;
+		multi = a * b;
 		suma = a + b;
 		div = a / b;
-		System.out.println("El resultado de 5+10 es " + suma + ", de 5-10 es " + resta + ", " + "de 5*10 es " + mult
+		System.out.println("El resultado de 5+10 es " + suma + ", de 5-10 es " + resta + ", " + "de 5*10 es " + multi
 				+ ", de 5/10 es " + div + " y de 5%10 es " + mod);
 	}
 
