c0485a2efd1ee73d4b6d74a092d8a3c807f5c24e
David Romero San Millán <david.romtrad@gmail.com>
2025-05-12 13:55:17 +0200

    Ejercicios 16

--- C3-BasesDatos/UD16-DMLII/Ej01Informatica.sql
+++ C3-BasesDatos/UD16-DMLII/Ej01Informatica.sql
@@ -15,59 +15,80 @@ INSERT INTO articulos (nombre, precio, fabricante) VALUES
     ('Disco duro SSD', 175, 7);
 --Ej 1.1. Obtener los nombres de los productos de la tienda.
 SELECT nombre FROM articulos;
+
 --Ej 1.2. Obtener los nombres y los precios de los productos de la tienda.
 SELECT nombre, precio FROM articulos;
+
 --Ej 1.3. Obtener el nombre de los productos cuyo precio seya igual o inferior a 200.
 SELECT nombre FROM articulos WHERE precio <= 200;
+
 --Ej 1.4. Obtener todos los datos de los artículos cuyo precio esté entre los 60 y 120 (ambos incluidos)
 SELECT * FROM articulos WHERE precio BETWEEN 60 AND 120;
+
 --Ej 1.5. Obtener el nombre y el precio en pesetas (precio * 166'386).
 SELECT nombre, precio*166.386 AS 'Precio en pesetas' FROM articulos;
+
 --Ej 1.6. Seleccionar el precio medio de todos los productos.
 SELECT AVG(precio) AS 'Precio medio' FROM articulos;
+
 --Ej 1.7. Obtener el precio medio de los artículos cuyo código de fabricante sea 2.
 SELECT AVG(precio) AS 'Precio medio de artículos AMD' FROM articulos WHERE fabricante=2;
+
 --Ej 1.8. Obtener el número de artículos cuyo precio sea mayor o igual a 180.
 SELECT COUNT(id_articulo) FROM articulos WHERE precio >= 180;
+
 --Ej 1.9. Obtener nombre y precio de artículos cuyo precio sea >=180 y ordenar DESC por precio, ASC por nombre.
 SELECT nombre, precio FROM articulos WHERE precio >= 180 ORDER BY precio DESC nombre ASC;
+
 --Ej 1.10. Obtener listado de artículos, incluyendo nombre, precio y datos del fabricante.
-SELECT a.nombre AS 'Articulo', precio AS 'Precio', f.id_fabricante AS 'ID Fabricante', f.nombre AS 'Fabricante' FROM articulos a
+SELECT a.nombre AS 'Articulo', precio AS 'Precio', f.id_fabricante 
+AS 'ID Fabricante', f.nombre AS 'Fabricante' FROM articulos a
 INNER JOIN fabricantes f
 ON a.fabricante = f.id_fabricante;
+
 --Ej 1.11. Obtener un listado de artículos, incluyendo el nombre del artículo, 
 --su precio y el nombre del fabricante.
 SELECT a.nombre AS 'Articulo', precio AS 'Precio', f.nombre AS 'Fabricante' FROM articulos a
 INNER JOIN fabricantes f
 ON a.fabricante = f.id_fabricante;
+
 --Ej 1.12. Obtener el precio medio de los productos de cada fabricante, mostrando solo los códigos de fabricante.
 SELECT AVG(precio) AS 'Precio medio', f.id_fabricante AS 'Fabricante' FROM articulos a
 INNER JOIN fabricantes f
 ON a.fabricante = f.id_fabricante
 GROUP BY fabricante;
+
 --Ej 1.13. Obtener el precio medio de los productos de cada fabricante, mostrando el nombre del fabricante.
 SELECT AVG(precio) AS 'Precio medio', f.nombre AS 'Fabricante' FROM articulos a
 INNER JOIN fabricantes f
 ON a.fabricante = f.id_fabricante
 GROUP BY fabricante;
+
 --Ej 1.14. Obtener los nombres de los fabricantes que ofrezcan productos cuyo precio medio sea mayor o igual a 150.
 SELECT f.nombre AS 'Fabricante', AVG(precio) AS 'Precio medio' FROM articulos a 
 INNER JOIN fabricantes f
 ON a.fabricante = f.id_fabricante
 WHERE precio >= 150
 GROUP BY fabricante;
+
 --Ej 1.15. Obtener el nombre y precio del artículo más barato.
 SELECT nombre AS 'Artículo más barato', precio AS 'Precio ' FROM articulos 
 WHERE precio = (SELECT MIN(precio) FROM articulos);
+
 --Ej 1.16. Obtener una lista con el nombre y precio de los artículos más caros
 --de cada proveedor (incluyendo el nombre del proveedor).
 SELECT nombre AS 'Artículo más caro', MAX(precio) AS 'Precio' FROM articulos
 GROUP BY fabricante;
+
 --Ej 1.17. Añadir un nuevo producto: Altavoces de 70€ del fabricante 2.
 INSERT INTO articulos (nombre, precio, fabricante) VALUES ('Altavoces', 70, 2);
+
 --Ej 1.18. Cambiar el nombre del producto 8 a 'Impresora Laser'.
 UPDATE articulos SET nombre='Impresora Laser'
 WHERE id_articulo=8;
+
 --Ej 1.19. Aplicar un descuento del 10% a todos los productos.
 UPDATE articulos SET precio=precio*0.9;
+
 --Ej 1.20. Aplicar un descuento de 10€ a todos los productos cuyo precio sea mayor o igual a 120.
+UPDATE articulos SET precio=precio-10 WHERE precio >= 120;
---
+++ C3-BasesDatos/UD16-DMLII/Ej02Empleados.sql
@@ -0,0 +1,96 @@
+INSERT INTO departamentos (id_departamento, nombre, presupuesto) VALUES
+(77, 'Investigación', 95000),
+(14, 'Informática', 45000),
+(21, 'Ventas', 85000),
+(37, 'Recursos humanos', 38000),
+(42, 'Dirección', 100000);
+
+INSERT INTO empleados (dni, nombre, apellidos, departamento) VALUES
+('76442098T', 'Juan', 'Soriano Mendoza', 14),
+('44900231K', 'Maria', 'Lagos Navaja', 77),
+('32008433L', 'Karen', 'Muñoz Vázquez', 14),
+('81112029J', 'Sílvia', 'Hinojoso Gómez', 21),
+('65526541N', 'Juan', 'López León', 77),
+('11032284V', 'Felicia', 'López Valdez', 14),
+('10422124Q', 'Marcos', 'Pérez Zambrano', 37),
+('08843765I', 'Maria', 'Carrión Mendoza', 77),
+('42210989F', 'Paula', 'Mendoza Pérez', 37),
+('65091165A', 'José', 'Castro Bermejo', 42);
+
+-- 2.1. Obtener los apellidos de los empleados.
+SELECT apellidos AS 'Apellidos' FROM empleados;
+
+-- 2.2. Obtener los apellidos de los empleados sin repeticiones.
+SELECT apellidos AS 'Apellidos sin repetir' FROM empleados
+GROUP BY apellidos;
+
+-- 2.3. Obtener todos los datos de los empleados que se apellidan 'López'.
+SELECT * FROM empleados WHERE apellidos='López';
+
+-- 2.4. Obtener todos los datos de los empleados que se apellidan 'López' y 'Pérez'.
+SELECT * FROM empleados WHERE apellidos IN ('López', 'Pérez');
+
+-- 2.5. Obtener todos los datos de los empleados que trabajan en el departamento 14.
+SELECT * FROM empleados WHERE departamento=14;
+
+-- 2.6. Obtener todos los datos de los empleados que trabajan para el departamento 37 y 77.
+SELECT * FROM empleados WHERE departamento IN (37, 77);
+
+-- 2.7. Obtener todos los datos de los empleados cuyo apellido comience por 'P'.
+SELECT * FROM empleados WHERE apellidos LIKE 'P%';
+
+-- 2.8. Obtener el presupuesto total de todos los departamentos.
+SELECT SUM(presupuesto) AS 'Presupuesto total' FROM departamentos;
+
+-- 2.9. Obtener el número de empleados de cada departamento.
+SELECT id_departamento AS 'Departamento', COUNT(dni) AS 'Número de empleados' FROM empleados 
+INNER JOIN departamentos ON departamento=id_departamento
+GROUP BY departamento;
+
+-- 2.10. Obtener un listado completo de empleados, incluyendo por cada empleado sus datos 
+-- y los de su departamento.
+SELECT dni AS 'DNI' , e.nombre AS 'Nombre', apellidos AS 'Apellidos', departamento AS 'Nº Departamento', 
+d.nombre AS 'Departamento', d.presupuesto AS 'Presupuesto' FROM empleados e
+INNER JOIN departamentos d ON departamento=id_departamento;
+
+-- 2.11. Obtener un listado completo de empleados, incluyendo el nombre y apellidos del empleado
+-- junto al nombre y presupuesto de su departamento.
+SELECT e.nombre AS 'Nombre', apellidos AS 'Apellidos', 
+d.nombre AS 'Departamento', d.presupuesto AS 'Presupuesto' FROM empleados e
+INNER JOIN departamentos d ON departamento=id_departamento;
+
+-- 2.12. Obtener los nombres y apellidos de los empleados que trabajen en departamentos 
+-- con presupuesto > 60.000.
+SELECT e.nombre AS 'Nombre', apellidos AS 'Apellidos', presupuesto AS 'Presupuesto'FROM empleados e 
+INNER JOIN departamentos ON departamento=id_departamento
+WHERE presupuesto > 60000;
+
+-- 2.13. Obtener los datos de los departamentos cuyo presupuesto es superior al presupuesto medio 
+-- de todos los departamentos.
+SELECT * FROM departamentos WHERE presupuesto > (SELECT AVG(presupuesto) FROM departamentos);
+
+-- 2.14. Obtener los nombres (únicamente) de los departamentos que tienen más de dos empleados.
+SELECT d.nombre AS 'Depts. con más de 2 empleados' FROM departamentos d
+INNER JOIN empleados ON departamento=id_departamento
+WHERE (SELECT COUNT(departamento) FROM empleados)
+GROUP BY d.nombre HAVING COUNT(departamento) > 2;
+
+-- 2.15. Añadir un nuevo departamento "Calidad", con presupuesto 40000 y código 11. 
+-- Añadir un empleado vinculado al departamento recién creado: Esther Vázquez, DNI: 89267109.
+INSERT INTO departamentos(id_departamento, nombre, presupuesto) VALUES
+(11, 'Calidad', 40000);
+INSERT INTO empleados(dni, nombre, apellidos, departamento) VALUES
+('89267109', 'Esther', 'Vázquez', 11);
+
+-- 2.16. Aplicar un recorte presupuestario del 10% a todos los departamentos.
+UPDATE departamentos SET presupuesto=presupuesto*0.9;
+
+-- 2.17. Reasignar a los empleados del departamento 77 al 14.
+UPDATE empleados SET departamento=14
+WHERE departamento = 77;
+
+-- 2.18. Despedir a todos los empleados del departamento 14.
+DELETE * FROM empleados WHERE departamento = 14;
+
+-- 2.19. Despedir a todos los empleados de los departamentos con presupuesto > 60000.
+    
