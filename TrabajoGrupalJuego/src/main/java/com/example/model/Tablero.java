fb3723f02726d2989c86d4f5e8b11867b3fc9a54
David Romero San Millán <david.romtrad@gmail.com>
2025-06-09 14:00:23 +0200

    grupal HLF

--- TrabajoGrupalJuego/src/main/java/com/example/model/Tablero.java
+++ TrabajoGrupalJuego/src/main/java/com/example/model/Tablero.java
@@ -1,47 +1,48 @@
 package com.example.model;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Tablero {
-    private List<Barco> barcos;
+	private List<Barco> barcos;
 
-    public Tablero() {
-        this.barcos = new ArrayList<>();
-    }
+	public Tablero() {
+		this.barcos = new ArrayList<>();
+	}
 
-    public void agregarBarco(Barco barco) {
-        barcos.add(barco);
-    }
-    public List<Barco> getBarcos() {
-        return barcos;
-    }
+	public void agregarBarco(Barco barco) {
+		barcos.add(barco);
+	}
 
-    public List<int[]> obtenerPosicionesDeBarcos() {
-        List<int[]> posiciones = new ArrayList<>();
-        for (Barco barco : barcos) {
-            posiciones.addAll(barco.getPosiciones());
-        }
-        return posiciones;
-    }
-    public boolean disparar(int fila, int columna) {
-        for (Barco barco : barcos) {
-            if (barco.contienePosicion(fila, columna)) {
-                return true; // Acierto
-            }
-        }
-        return false; // Fallo
-    }
+	public List<Barco> getBarcos() {
+		return barcos;
+	}
 
-    public boolean esImpacto(int x, int y) {
-        for (Barco barco : barcos) { // barcos es la lista de barcos en el tablero
-            for (int[] posicion : barco.getPosiciones()) {
-                if (posicion[0] == x && posicion[1] == y) {
-                    return true; // Hay un barco en esta posición
-                }
-            }
-        }
-        return false; // No hay barco en esta posición
-    }
+	public List<int[]> obtenerPosicionesDeBarcos() {
+		List<int[]> posiciones = new ArrayList<>();
+		for (Barco barco : barcos) {
+			posiciones.addAll(barco.getPosiciones());
+		}
+		return posiciones;
+	}
+
+	public boolean disparar(int fila, int columna) {
+		for (Barco barco : barcos) {
+			if (barco.contienePosicion(fila, columna)) {
+				return true; // Acierto
+			}
+		}
+		return false; // Fallo
+	}
+
+	public boolean esImpacto(int x, int y) {
+		for (Barco barco : barcos) { // barcos es la lista de barcos en el tablero
+			for (int[] posicion : barco.getPosiciones()) {
+				if (posicion[0] == x && posicion[1] == y) {
+					return true; // Hay un barco en esta posición
+				}
+			}
+		}
+		return false; // No hay barco en esta posición
+	}
 }
